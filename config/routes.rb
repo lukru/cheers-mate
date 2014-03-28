54e5f084449e03b5d10625f7d64e50d7475ee69a
Lauren Krueger <lukru489@gmail.com>
2014-03-28 13:15:15 +1100

    more styling and added about page that links in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ CheersMate::Application.routes.draw do
 
   root :to => "drinks#index"
 
+  get "/:action", to: "pages#:action"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
